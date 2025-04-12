db4fd848ee1f0087ba9368d153fb0ce2053d45d4
Omer Aydeniz <omer.aydeniz@outlook.com.tr>
2025-04-12 20:31:55 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function filter(category) {
+    const items = document.querySelectorAll('.item');
+    items.forEach(item => {
+        if (category === 'all' || item.classList.contains(category)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
