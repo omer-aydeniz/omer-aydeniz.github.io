435ef2f8df0c8b9859f56fba39da55c6a4d710d2
Omer Aydeniz <omer.aydeniz@outlook.com.tr>
2025-04-12 21:36:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function filter(category) {
-    const items = document.querySelectorAll('.item');
-    items.forEach(item => {
-        if (category === 'all' || item.classList.contains(category)) {
-            item.style.display = 'block';
-        } else {
-            item.style.display = 'none';
-        }
-    });
-}
-document.querySelector('a[href="#hakkimizda"]').addEventListener('click', () => {
-    document.getElementById('hakkimizda').scrollIntoView({ behavior: 'smooth' });
-});
 
